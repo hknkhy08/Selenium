0498a6ff0be799a9d0dee2b5c595b28f8faa0a23
Hakan Kahya <kahyahakan08@gmail.com>
2019-06-20 17:46:32 -0500

    first commit

---
+++ Documents/eclipse-workspace/Selenium/src/testSel/test1.java
@@ -0,0 +1,33 @@
+package testSel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/hakankahya/Documents/selenium_dependencies/drivers/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://wwww.google.com");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
